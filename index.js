0bd5e1819531fe38168d64130b5b165a7443bdc9
Key <markpauleyjr@gmail.com>
2023-06-23 22:46:07 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//import { messagesData } from './data.js'
-//import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
@@ -18,57 +16,32 @@ document.addEventListener('click', function(e){
 
 function handleMessageBtnClick(){
     const messageInput = document.getElementById('message-input')
+    const nameInput = document.getElementById('name-input')
     if(messageInput.value){
         push(messagesInDB, {
-            handle: `@Scrimba`,
+            handle: nameInput.value,
             messageText: messageInput.value,
         })
-    //render()
     messageInput.value = ''
     }
 
 }
 
-function getFeedHtml(){
-    let feedHtml = ``
-    
-    messagesInDB.forEach(function(message){
-        
-         feedHtml += `
-        <div class="message">
-            <div class="message-inner">
-                <img src="${message.profilePic}" class="profile-pic">
-                <div>
-                    <p class="handle">${message.handle}</p>
-                    <p class="message-text">${message.messageText}</p>
-                </div>            
-            </div>
-            <!-- <div class="hidden" id="replies-${message.uuid}"> -->
-            </div>   
-        </div>
-        `
-   })
-   return feedHtml 
-}
-
-function render(){
-    document.getElementById('feed').innerHTML = getFeedHtml()
-}
+function scrollToBottom() {
+    const body = document.querySelector('body');
+    body.scrollTop = body.scrollHeight;
+  }
 
 onValue(messagesInDB, function(snapshot) {
     
     let feedHtml = ``
     
     snapshot.forEach(function(childSnapshot) {
-    var messageKey = childSnapshot.key
+    //var messageKey = childSnapshot.key
     var messageData = childSnapshot.val()
     var messageHandle = messageData.handle
     var messageText = messageData.messageText
     
-    // console.log("Key:", messageKey)
-    // console.log("Handle:", messageHandle)
-    // console.log("Message Text:", messageText)
-
     feedHtml += `
     <div class="message">
         <div class="message-inner">
@@ -77,14 +50,11 @@ onValue(messagesInDB, function(snapshot) {
                 <p class="message-text">${messageText}</p>
             </div>            
         </div>
-        <!-- <div class="hidden" id="replies-${messageKey}"> -->
         </div>   
     </div>
     `
 
     });
     document.getElementById('feed').innerHTML = feedHtml
+    scrollToBottom()
 })
-
-//render()
-
